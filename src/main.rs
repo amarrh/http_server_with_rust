d01965c004663bac0746117bdd713a1cde5c0e9a
Amar Hodžić <66332497+amarrh@users.noreply.github.com>
2021-09-04 13:02:14 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,3 @@ fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run(WebsiteHandler::new(public_path));
 }
-
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
-*/

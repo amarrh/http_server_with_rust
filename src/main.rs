27ea8b664edd8af7b7e3b67e9cc5bae09c2e0674
Amar Hodžić <66332497+amarrh@users.noreply.github.com>
2021-09-04 13:01:57 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod website_handler;
 fn main() {
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR")); //CARGO_MANIFEST_DIR - pwd path on a specific machine
     let public_path =  env::var("PUBLIC_PATH").unwrap_or(default_path);
-    println!("public path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run(WebsiteHandler::new(public_path));
 }
